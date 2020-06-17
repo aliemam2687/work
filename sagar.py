8264a6a6d39ecf5ea75aeca2d948a90418df8846
Ali-Emam-Al-Badi <63938443+aliemam2687@users.noreply.github.com>
2020-06-18 04:04:57 +0600

    another compliment

--- sagar.py
+++ sagar.py
@@ -2,3 +2,6 @@ print("you are awesome")
 print(" you are absolutely genious")
 print("you can do it")
 print("never let you down")
+#now i will put some more compliment
+print("your success gives me a thought")
+5+6
